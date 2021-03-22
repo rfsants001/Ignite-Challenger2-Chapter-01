720353cda41ae4361141450e6b34a616fed3131d
rfsants001 <rf.sants001@gmail.com>
2021-03-22 12:24:44 -0300

    Solved

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,38 @@
-export function Content() {
-  // Complete aqui
+
+import { MovieCard } from './MovieCard';
+
+import '../styles/content.scss';
+interface ContentProps {
+  selectedGenre: { title: string };
+  movies: Array<{
+    imdbID: string,
+    Title: string;
+    Poster: string;
+    Runtime: string;
+    Ratings: Array<{ Value: string }>;
+  }>;
+}
+
+interface GenreResponseProps {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+}
+
+export function Content({movies, selectedGenre}:ContentProps) {
+  return(
+      <div className="container">
+        <header>
+          <span className="category">Categoria:<span> {selectedGenre.title}</span></span>
+        </header>
+
+        <main>
+          <div className="movies-list">
+            {movies.map(movie => (
+              <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+            ))}
+          </div>
+        </main>
+      </div>
+  )
 }
